ecc800e03db1ed815e554f7072fed9144ac8d37d
FaixaTheGutipoka <faixa.the.gutipoka@gmail.com>
2023-11-28 01:43:24 +0600

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+public class Main extends Application{
+    @Override
+    public void start(Stage primaryStage){
+        Vbox root = new Vbox
+    }
     public static void main(String[] args) {
 
     }
