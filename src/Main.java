ff7c9091039db5aa66d0e17d3981ac7216f93307
FaixaTheGutipoka <faixa.the.gutipoka@gmail.com>
2023-11-29 19:42:03 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
 public class Main extends Application{
     @Override
     public void start(Stage primaryStage){
-        Vbox root = new Vbox
+        VBox root = new VBox();
+
     }
     public static void main(String[] args) {
 
